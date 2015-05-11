0cd4f91ebf7107660f5e03dc505a6f4579b1d326
Andrés Javier López <ajavier.lopez@gmail.com>
2015-05-11 09:09:47 -0600

    Added documentation to the main module

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,16 @@
+"""Creates and shows the movie website.
+
+This is the main module for the creation of the movie website. The list of
+movies are loaded from the movies module and the process to create the site is
+loaded from the fresh_tomatoes module.
+
+This module only executes itself when is called as the main module, and won't
+do anything when imported.
+"""
+
 import fresh_tomatoes
 import movies
 
 
 if __name__ == '__main__':
     fresh_tomatoes.open_movies_page(movies.movies)
-
